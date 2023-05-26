a12e967861f772aa84451dd7e80221c65ec9ae29
nagababu <nag4c1@gmail.com>
2023-05-26 17:35:48 +0530

    frontend code

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,18 @@
 echo -e "\e[31m Install nginx \e[0m"
-yum install nginx -y
+yum install nginx -y &>>/tmp/logs
 
 echo -e "\e[31m Removing default content \e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/logs
 
 echo -e "\e[31m Downloading frontend content \e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/logs
 
 echo -e "\e[31m Changing directory to html \e[0m"
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>/tmp/logs
 
 echo -e "\e[31m Unzipping the frontend files \e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/logs
 
 echo -e "\e[31m Restart Nginx \e[0m"
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>/tmp/logs
+systemctl restart nginx &>>/tmp/logs
