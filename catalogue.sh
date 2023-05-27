113972b31d5d4531127adc738baee53904dd7c09
nagababu <nag4c1@gmail.com>
2023-05-27 12:52:01 +0530

    frontend code

--- catalogue.sh
+++ catalogue.sh
@@ -22,13 +22,6 @@ echo -e "\e[32m Installing dependencies \e[0m"
 cd /app &>>/tmp/logs
 npm install &>>/tmp/logs
 
-echo -e "\e[32m Installing mongodb \e[0m"
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/logs
-yum install mongodb-org-shell -y &>>/tmp/logs
-
-echo -e "\e[32m Loading schema \e[0m"
-mongo --host mongodb-dev.devopshemasri.online </app/schema/catalogue.js &>>/tmp/logs
-
 echo -e "\e[32m Coping catalogue service file \e[0m"
 cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>/tmp/logs
 
@@ -36,3 +29,11 @@ echo -e "\e[32m Restarting catalogue service \e[0m"
 systemctl daemon-reload &>>/tmp/logs
 systemctl enable catalogue &>>/tmp/logs
 systemctl restart catalogue &>>/tmp/logs
+
+echo -e "\e[32m Installing mongodb \e[0m"
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/logs
+yum install mongodb-org-shell -y &>>/tmp/logs
+
+echo -e "\e[32m Loading schema \e[0m"
+mongo --host mongodb-dev.devopshemasri.online </app/schema/catalogue.js &>>/tmp/logs
+
